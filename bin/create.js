0fb6054fb67513919b465aad177d6582759b4dd0
张大亨 <zhangdaheng@zhangdahengdeMacBook-Pro.local>
2019-08-08 13:32:37 +0800

    添加下载模版功能

--- bin/create.js
+++ bin/create.js
@@ -1,33 +1,40 @@
-const inquirer = require('inquirer')
+const ora = require('ora')
 const path = require('path')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const clone = require('download-git-repo')
+const { execSync } = require('child_process')
 
 module.exports = async (name, cmd) => {
 	const projectName = path.relative('../', process.cwd())
-    const { radio, checkbox } = await inquirer.prompt([
+    const { template } = await inquirer.prompt([
 		{
-			name: 'radio',
+			name: 'template',
 			type: 'list',
 			message: `单选`,
 			choices: [
-				{ name: '单选1', value: 'radio1'},
-				{ name: '单选2', value: 'radio2'},
-				{ name: 'Cancel', value: false }
-			]
-		},
-		{
-			name: 'checkbox',
-			type: 'checkbox',
-			message: `多选`,
-			choices: [
-				{ name: '多选1', value: 'checkbox1'},
-				{ name: '多选2', value: 'checkbox2'},
-				{ name: '多选3', value: 'checkbox3'},
-				{ name: '多选4', value: 'checkbox4'},
+				{ name: '不使用', value: false},
+				{ name: '简易模版', value: 'simple-template'}
 			]
 		}
 	])
 
-	console.log(`正在创建项目${name === '.' ? projectName : name}，所带参数有${JSON.stringify(cmd)}`)
-	console.log('radio', radio)
-	console.log('checkbox', checkbox)
+	if (template) {
+		let spinner = ora('downloading template ...')
+		spinner.start()
+		clone(
+			`direct:https://github.com/DahengZhang/daheng-cli.git#${template}`,
+			name === '.' ? '.' : `./${name}`, { clone: true }, function(err) {
+				if (err) {
+					spinner.fail()
+					console.log(chalk.red(err))
+					return
+				}
+				spinner.succeed()
+				console.log(chalk.green('模版下载完成'))
+			}
+		)
+	} else {
+		console.log(chalk.yellow('不实用任何模版'))
+	}
 }
